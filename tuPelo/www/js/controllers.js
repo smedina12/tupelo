64831dd84fc1c18656abd472ef43dcee425f888a
Samuel Medina <samuelmedina1991@gmail.com>
2016-10-18 17:53:27 +0000

    Testing some new routes

--- tuPelo/www/js/controllers.js
+++ tuPelo/www/js/controllers.js
@@ -103,3 +103,70 @@ function ($scope, $stateParams) {
 
 
 }])
+
+
+
+.controller('signUpCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cartTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cloudTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+      
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('customerSignUpCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('welcomePageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('hairstylistCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
--- tuPelo/www/js/routes.js
+++ tuPelo/www/js/routes.js
@@ -1,7 +1,9 @@
 
+var user = 'Owner';
+if(user != 'Owner')
+{
 
 angular.module('app.routes', [])
-
 .config(function($stateProvider, $urlRouterProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
@@ -12,57 +14,110 @@ angular.module('app.routes', [])
     
   
 
-      .state('employeeMenu.employeeAppointments', {
-    url: '/employeeAppointments',
+      .state('menu.settings', {
+    url: '/settings',
     views: {
       'side-menu21': {
-        templateUrl: 'employee_view/employeeAppointments.html',
-        controller: 'employeeAppointmentsCtrl'
+        templateUrl: 'manager_view/settings.html',
+        controller: 'settingsCtrl'
       }
     }
   })
 
-  .state('employeeMenu.employeeCalendar', {
-    url: '/employeeCalendar',
+  .state('menu.employees', {
+    url: '/Manager_Employee',
     views: {
       'side-menu21': {
-        templateUrl: 'employee_view/employeeCalendar.html',
-        controller: 'employeeCalendarCtrl'
+        templateUrl: 'manager_view/employees.html',
+        controller: 'employeesCtrl'
       }
     }
   })
 
-  .state('employeeMenu.employeeReviews', {
-    url: '/employeeReviews',
+  .state('menu.fullCalendar', {
+    url: '/Manager_Fullcalendar',
     views: {
       'side-menu21': {
-        templateUrl: 'employee_view/employeeReviews.html',
-        controller: 'employeeReviewsCtrl'
+        templateUrl: 'manager_view/fullCalendar.html',
+        controller: 'fullCalendarCtrl'
       }
     }
   })
 
-  .state('employeeMenu.employeeCustomerSettings', {
-    url: '/employeeCustomerSettings',
+  .state('menu.myCalendar', {
+    url: '/Manager_Mycalendar',
     views: {
       'side-menu21': {
-        templateUrl: 'employee_view/employeeCustomerSettings.html',
-        controller: 'employeeCustomerSettingsCtrl'
+        templateUrl: 'manager_view/myCalendar.html',
+        controller: 'myCalendarCtrl'
       }
     }
   })
 
-  .state('employeeMenu', {
-    url: '/employeeMenu',
-    templateUrl: 'employee_view/employeeMenu.html',
-    controller: 'employeeMenuCtrl'
+  .state('menu.detailedCalendar', {
+    url: '/Manager_DetailCalendar',
+    views: {
+      'side-menu21': {
+        templateUrl: 'manager_view/detailedCalendar.html',
+        controller: 'detailedCalendarCtrl'
+      }
+    }
   })
 
-$urlRouterProvider.otherwise('/employeeMenu/employeeAppointments')
+  .state('menu', {
+    url: '/Manager_sideMenu',
+    templateUrl: 'manager_view/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('menu.employeeAdd', {
+    url: '/Add_Employee',
+    views: {
+      'side-menu21': {
+        templateUrl: 'manager_view/employeeAdd.html',
+        controller: 'employeeAddCtrl'
+      }
+    }
+  })
+
+  .state('menu.test1', {
+    url: '/justTest',
+    views: {
+      'side-menu21': {
+        templateUrl: 'manager_view/test1.html',
+        controller: 'test1Ctrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/Manager_sideMenu/Manager_Mycalendar')
 
   
 
 });
+}
+else
+{
+  angular.module('app.routes', [])
+.config(function($stateProvider, $urlRouterProvider) {
 
-
-
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+  .state('menu.settings', {
+    url: '/settings',
+    views: {
+      'side-menu21': {
+        templateUrl: 'manager_view/settings.html',
+        controller: 'settingsCtrl'
+      }
+    }
+  })
+  $urlRouterProvider.otherwise('/Manager_sideMenu/Manager_Mycalendar')
+});
+  
+var node = document.getElementById('hi');
+node.innerHTML('<p>some dynamic html</p>');
+}
