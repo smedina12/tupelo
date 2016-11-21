0b731524db34b0823d56f2776be2a12e887325d9
Samuel Medina <samuelmedina1991@gmail.com>
2016-11-21 05:41:47 +0000

    Adding disable dates and times

--- tuPelo/www/js/controllers.js
+++ tuPelo/www/js/controllers.js
@@ -291,6 +291,45 @@ $scope.data = {
         alert('Form submitted');
     };
     
+/* Bindable functions
+ -----------------------------------------------*/
+$scope.endDateBeforeRender = endDateBeforeRender
+$scope.endDateOnSetTime = endDateOnSetTime
+$scope.startDateBeforeRender = startDateBeforeRender
+$scope.startDateOnSetTime = startDateOnSetTime
+
+function startDateOnSetTime () {
+  $scope.$broadcast('start-date-changed');
+}
+
+function endDateOnSetTime () {
+  $scope.$broadcast('end-date-changed');
+}
+
+function startDateBeforeRender ($dates) {
+  if ($scope.dateRangeEnd) {
+    var activeDate = moment($scope.dateRangeEnd);
+
+    $dates.filter(function (date) {
+      return date.localDateValue() >= activeDate.valueOf()
+    }).forEach(function (date) {
+      date.selectable = false;
+    })
+  }
+}
+
+function endDateBeforeRender ($view, $dates) {
+  if ($scope.dateRangeStart) {
+    var activeDate = moment($scope.dateRangeStart).subtract(1, $view).add(1, 'minute');
+
+    $dates.filter(function (date) {
+      return date.localDateValue() <= activeDate.valueOf()
+    }).forEach(function (date) {
+      date.selectable = false;
+    })
+  }
+}
+
     
     
     
